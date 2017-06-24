ecbbd8b7c53ef0d4926da20320d4c5e35373728c
Joe Huss <detain@interserver.net>
2017-06-24 07:19:37 -0400

    camelCase type refactoring

--- src/Plugin.php
+++ src/Plugin.php
@@ -27,7 +27,7 @@ class Plugin {
 		$service = $event->getSubject();
 		function_requirements('class.Addon');
 		$addon = new \Addon();
-		$addon->set_module('vps')
+		$addon->setModule('vps')
 			->set_text('Additional IP')
 			->set_text_match('Additional IP (.*)')
 			->set_cost(VPS_IP_COST)
