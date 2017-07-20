50c3bac3b9296801b13c3e26a0e6c5f3ad615164
Joe Huss <detain@interserver.net>
2017-07-20 00:48:31 -0400

    a bunch of fixs tweaks and iprovements

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,6 +4,11 @@ namespace Detain\MyAdminVpsIps;
 
 use Symfony\Component\EventDispatcher\GenericEvent;
 
+/**
+ * Class Plugin
+ *
+ * @package Detain\MyAdminVpsIps
+ */
 class Plugin {
 
 	public static $name = 'Additional IPs VPS Addon';
@@ -12,10 +17,15 @@ class Plugin {
 	public static $module = 'vps';
 	public static $type = 'addon';
 
-
+	/**
+	 * Plugin constructor.
+	 */
 	public function __construct() {
 	}
 
+	/**
+	 * @return array
+	 */
 	public static function getHooks() {
 		return [
 			self::$module.'.load_addons' => [__CLASS__, 'getAddon'],
@@ -23,6 +33,9 @@ class Plugin {
 		];
 	}
 
+	/**
+	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+	 */
 	public static function getAddon(GenericEvent $event) {
 		$service = $event->getSubject();
 		function_requirements('class.Addon');
@@ -38,6 +51,11 @@ class Plugin {
 		$service->addAddon($addon);
 	}
 
+	/**
+	 * @param \Service_Order $serviceOrder
+	 * @param                $repeatInvoiceId
+	 * @param bool           $regexMatch
+	 */
 	public static function doEnable(\Service_Order $serviceOrder, $repeatInvoiceId, $regexMatch = FALSE) {
 		$serviceInfo = $serviceOrder->getServiceInfo();
 		$settings = get_module_settings(self::$module);
@@ -70,6 +88,11 @@ class Plugin {
 		}
 	}
 
+	/**
+	 * @param \Service_Order $serviceOrder
+	 * @param                $repeatInvoiceId
+	 * @param bool           $regexMatch
+	 */
 	public static function doDisable(\Service_Order $serviceOrder, $repeatInvoiceId, $regexMatch = FALSE) {
 		$serviceInfo = $serviceOrder->getServiceInfo();
 		$settings = get_module_settings(self::$module);
@@ -91,6 +114,9 @@ class Plugin {
 		admin_mail($subject, $email, $headers, FALSE, 'admin_email_vps_ip_canceled.tpl');
 	}
 
+	/**
+	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+	 */
 	public static function getSettings(GenericEvent $event) {
 		$settings = $event->getSubject();
 		$settings->add_text_setting(self::$module, 'Addon Costs', 'vps_ip_cost', 'VPS Additional IP Cost:', 'This is the cost for purchasing an additional IP on top of a VPS.', $settings->get_setting('VPS_IP_COST'));
